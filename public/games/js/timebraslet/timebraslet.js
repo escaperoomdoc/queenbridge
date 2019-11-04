c8229b411451dbc91e85ab055952ec09fb71bf43
dsi <wld_h@mail.ru>
2019-11-04 22:44:15 +0300

    123

--- public/games/js/timebraslet/timebraslet.js
+++ public/games/js/timebraslet/timebraslet.js
@@ -3,122 +3,79 @@ qb = new QueenBridge(location.host, {id: 'timebraslet-'+gameParams, autoping: nu
 var app = new PIXI.Application(1280, 720, { transparent: true });
 document.body.appendChild(app.view);
 
-/*
-// Create play button that can be used to trigger the video
-var button = PIXI.Sprite.fromImage('js/timebraslet/poweron.png');
-button.anchor.set(0.5);
-button.x = (app.screen.width - button.width) / 2;
-button.y = (app.screen.height - button.height) / 2;
-button.interactive = true;
-button.buttonMode = true;
-app.stage.addChild(button);
-button.on('pointertap', onStart);
-var hackerStage = {};
-hackerStage.socket = null;
+// screen size is constant
+const screenWidth = 1280;
+const screenHeight = 720;
 
-function onStart() {
-    button.destroy();
-*/
-	// screen size is constant
-	const screenWidth = 1280;
-	const screenHeight = 720;
-	const contentRect = {x: 240, y:180, w: 850, h: 320};
-
-	// text style is constant
-	var textStyle = new PIXI.TextStyle({
-		fontFamily: 'Algerian',	//'Berlin Sans FB', 'Candara'
-		fontSize: 128,
-		fontStyle: 'normal',
-		fontWeight: 'normal',
-		fill: ['#28ff20']
-	});
-
-	// create a new Sprite from an image path.
-	var Frame = function() {
-		// init members
-		this.bg = PIXI.Sprite.fromImage('js/timebraslet/bg.jpg');
-		this.bg.anchor.set(0.5);
-		this.bg.x = screenWidth / 2;
-		this.bg.y = screenHeight / 2;
+// create a new Sprite from an image path.
+var Frame = function() {
+	// init members
+	this.bg = PIXI.Sprite.fromImage('js/timebraslet/bg.jpg');
+	this.bg.anchor.set(0.5);
+	this.bg.x = screenWidth / 2;
+	this.bg.y = screenHeight / 2;
+	this.activeSprites = false;
+	this.stageCreate = function() {
+		this.activeSprites = true;
+		this.bg.hackerProperty = "frame";
+		app.stage.addChild(this.bg);
+	}
+	this.stageClear = function() {
 		this.activeSprites = false;
-		this.stageCreate = function() {
-			this.activeSprites = true;
-			this.bg.hackerProperty = "frame";
-			app.stage.addChild(this.bg);
-		}
-		this.stageClear = function() {
-			this.activeSprites = false;
-			for (var i = app.stage.children.length - 1; i >= 0; i--) {
-				if (app.stage.children[i].hackerProperty === "frame") app.stage.removeChild(app.stage.children[i]);
-			};	
-		}
-		// animate mask
-		this.animate = function() {
-			/*
-			if (this.activeSprites) {
-				this.mask.x = this.mask.x + this.maskcoords[this.maskcoord_step].dx;
-				this.mask.y = this.mask.y + this.maskcoords[this.maskcoord_step].dy;
-				if (this.mask.x == this.maskcoords[(this.maskcoord_step + 1) % 4].x) {
-					if (this.mask.y == this.maskcoords[(this.maskcoord_step + 1) % 4].y) {
-						this.maskcoord_step = (this.maskcoord_step + 1) % 4;
-					}
-				}
-			}
-			*/
-		}
-	};
+		for (var i = app.stage.children.length - 1; i >= 0; i--) {
+			if (app.stage.children[i].hackerProperty === "frame") app.stage.removeChild(app.stage.children[i]);
+		};	
+	}
+	// animate mask
+	this.animate = function() {
+	}
+};
 
-	var Subtitle = function(initialText) {
-		// init members
-		this.text = initialText;
+var textStyleTime = new PIXI.TextStyle({
+	fontFamily: 'Candara',	//'Berlin Sans FB', 'Candara'
+	fontSize: 128,
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fill: ['#28ff20']
+});
+
+var textStylePass = new PIXI.TextStyle({
+	fontFamily: 'Candara',	//'Berlin Sans FB', 'Candara'
+	fontSize: 128,
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	fill: ['#ffff20']
+});
+
+var textTime = function(text) {
+	// init members
+	this.textSprite = new PIXI.Text(this.text, textStyleTime);
+	this.textSprite.x = 200;
+	this.textSprite.y = 240;
+	// set new text
+	this.setText = function(text) {
+		this.text = text
 		this.textSize = this.text.length;
-		this.currentChar = 0;
-		this.animationCycle = 0;
-		this.textSprite = new PIXI.Text(this.text, textStyle);
-		this.textSprite.x = 200;
-		this.textSprite.y = 240;
-		this.textDelay = 200;
-		this.retype = false;
-		// set new text
-		this.setText = function(initialText, textDelay, retype) {
-			this.retype = retype;
-			this.text = initialText
-			this.textSize = this.text.length;
-			this.currentChar = 0;
-			this.animationCycle = 0;
-			this.textSprite.text = "";
-			this.textDelay = textDelay;
-		}
+		this.textSprite.text = text;
+	}
+	this.setText(text);
+	this.activeSprites = false;
+	// stageIn and stage Out
+	this.stageCreate = function() {
+		this.activeSprites = true;
+		this.textSprite.hackerProperty = "time";
+		app.stage.addChild(this.textSprite);
+	}
+	this.stageClear = function() {
 		this.activeSprites = false;
-		// stageIn and stage Out
-		this.stageCreate = function() {
-			this.activeSprites = true;
-			this.textSprite.hackerProperty = "subtitle";
-			this.textSprite.text = this.text;
-			app.stage.addChild(this.textSprite);
-			this.tmprev = performance.now();
-		}
-		this.stageClear = function() {
-			this.activeSprites = false;
-			for (var i = app.stage.children.length - 1; i >= 0; i--) {
-				if (app.stage.children[i].hackerProperty === "subtitle") app.stage.removeChild(app.stage.children[i]);
-			};	
-		}
-		this.animate = function() {
-			this.textSprite._style._fill[0] = ['#28ff20'];
-			/*
-			if (this.activeSprites) {
-				if (!this.retype && this.currentChar >= this.textSize - 1) return;
-				if (performance.now() - this.tmprev >= this.textDelay) {
-					this.tmprev = performance.now();
-					this.currentChar ++;
-					this.textSprite.text = this.text.substring(0, this.currentChar) + "_";
-					if (this.retype && this.currentChar >= this.textSize - 1) this.currentChar = 0;
-				}
-			}
-			*/
-		}
-	};
+		for (var i = app.stage.children.length - 1; i >= 0; i--) {
+			if (app.stage.children[i].hackerProperty === "time") app.stage.removeChild(app.stage.children[i]);
+		};	
+	}
+	this.animate = function() {
+		this.textSprite._style._fill[0] = ['#28ff20'];
+	}
+};
 
 	/*
 	var Codes = function(codesArray, fixTimes) {
@@ -378,7 +335,7 @@ function onStart() {
 	
 	// construction
 	frame = new Frame();
-	subtitle = new Subtitle('2568∙12∙25  13∙45∙06');
+	time = new textTime('2568∙12∙25 13∙45∙06');
 	/*
 	stick = new Stick();
 	securityVideo = new Video('assets/security.mp4', true);
@@ -390,12 +347,12 @@ function onStart() {
 	*/
 	// frame is always on
 	frame.stageCreate();
-	subtitle.stageCreate();
+	time.stageCreate();
 
 	// timer event
 	app.ticker.add(function() {
 		frame.animate();
-		subtitle.animate();
+		time.animate();
 		/*
 		stick.animate();
 		firewall.animate();
